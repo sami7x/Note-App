0b9abf4618374cd7a0ecab29d13a0722b604427e
vajrasami <vajrsami@gmail.com>
2024-01-16 22:10:42 +0545

    Google Auth Feature Added

--- app.js
+++ app.js
@@ -1,40 +1,56 @@
-require('dotenv').config();
+require("dotenv").config();
 
-//importing modules
+// importing modules
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
-const connectDB = require("./server/config/db"); 
+const connectDB = require("./server/config/db");
+const session = require("express-session");
+const passport = require("passport");
+const MongoStore = require("connect-mongo");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-//middleware
-app.use(express.urlencoded({extended:true}));
+app.use(
+  session({
+    secret: "sami bajracharya",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//connect to db
+// connect to database
 connectDB();
 
-//static files
+// static files
 app.use(express.static("public"));
 
-//Templating Engine
+// Templating Engine
 app.use(expressLayouts);
 app.set("layout", "./layouts/main");
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-//routes
+// routes
+app.use("/", require("./server/routes/auth"));
 app.use("/", require("./server/routes/index"));
 app.use("/", require("./server/routes/dashboard"));
 
-//404 Page Not Found
-app.get("*", function(req,res)
-{
-    // res.status(404).send("404 page not found");
-    res.status(404).render("404");
+// 404 page not found
+app.get("*", function (req, res) {
+  // res.status(404).send("404 page not found");
+  res.status(404).render("404");
 });
 
-app.listen(port,()=> {
-    console.log(`App listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
 });
-
